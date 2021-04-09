8a897c85f676fd982fef2dab39209a60f4de3f5e
thesakshipandey <54810583+thesakshipandey@users.noreply.github.com>
2021-04-09 23:43:43 +0530

    removing secret key

--- RGIT/settings.py
+++ RGIT/settings.py
@@ -18,7 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g7xn+oy#z(35wh90o%f0$)c1(-t=i*rc+s&1atoa7peto#e-mj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
