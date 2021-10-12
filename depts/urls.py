e4c11f1e16366dbc3d829470c44d572da313de27
thesakshipandey <54810583+thesakshipandey@users.noreply.github.com>
2021-10-12 16:13:16 +0530

    new IT staff

--- depts/urls.py
+++ depts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('main/', views.home, name = "home"),
+    path('', views.home, name = "home"),
     path('comp/', views.comp, name = "comp"),
     path('IT/', views.IT, name = "IT"),
     path('EXTC/', views.EXTC, name = "EXTC"),
@@ -11,5 +11,6 @@ urlpatterns = [
     path('instru/', views.instru, name = "instru"),
     path('about/', views.about, name = "about"),
     path('dept/', views.dept, name = "dept"),
-    path('comp/staff/', views.compstaff, name = "compstaff")
+    path('comp/staff/', views.compstaff, name = "compstaff"),
+    path('IT/staff/', views.ITstaff, name = "ITstaff")
 ]
